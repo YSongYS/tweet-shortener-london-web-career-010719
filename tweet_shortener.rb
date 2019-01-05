515fcfe5406914c816be12de7f4ac1fe4a4ea512
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-05 18:14:38 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,40 @@
 # Write your code here.
+def dictionary (lookup)
+  replace = lookup
+  dictionary = {
+    "hello" => 'hi',
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    'you' => "u",
+    "at" => "@",
+    "and" => "&"}
+  dictionary.each do |key,value|
+    replace = value if key == lookup
+  end
+  replace
+end
+
+def word_substituter(tweet)
+  new_tweet = tweet.split(" ").collect{|word| word=dictionary(word)}.join(" ")
+  new_tweet
+end
+
+def bulk_tweet_shortener(tweets)
+  i=0
+  new_tweets = []
+  while i<tweets.size do
+    new_tweets[i] = word_substituter(tweets[i])
+    i += 1
+  end
+  #tweets.map do |tweet|
+  #  tweet = word_substituter(tweet)
+  #end
+  puts new_tweets
+end
+
+bulk_tweet_shortener(["you and me", "me and you"])
+word_substituter("you and me")
