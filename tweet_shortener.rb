dbab83775a57a5acd4ddb63273f283181c36ca24
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-05 18:20:59 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -28,7 +28,7 @@ def bulk_tweet_shortener(tweets)
   new_tweets = []
   while i<tweets.size do
     new_tweets[i] = word_substituter(tweets[i])
-    print new_tweets[i]
+    puts new_tweets[i]
     i += 1
   end
   new_tweets
